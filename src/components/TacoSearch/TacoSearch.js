b7bd71aee5e0a304d361272972061765acb75b4b
Brady Bridges <bradyjbridges@gmail.com>
2019-12-18 19:19:47 -0700

    Add SearchBar styling

--- src/components/TacoSearch/TacoSearch.js
+++ src/components/TacoSearch/TacoSearch.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Dimensions, StyleSheet } from 'react-native';
+import { Text, View, Dimensions, StyleSheet, Image } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import * as Font from 'expo-font';
 
@@ -26,23 +26,39 @@ export default class TacoSearch extends Component {
           placeholder="Find Tacos..."
           onChangeText={this.handleChange}
           value={search}
-          lightTheme={true}
           containerStyle={styles.searchBar}
           inputStyle={dynamicInputStyles}
+          placeholderTextColor="black"
+          searchIcon={<Image source={require('../../../assets/007-search.png')} style={{width: 45, height: 45}} />}
+          inputContainerStyle={styles.inputContainer}
+          leftIconContainerStyle={{ marginLeft: 10 }}
         />
-      
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   searchBar: {
-    borderRadius: 4,
     width: Dimensions.get('window').width * .9,
+    padding: 0,
+    height: 60,
+    backgroundColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderTopColor: 'transparent'
   },
   input: {
     textAlign: 'center',
-    fontSize: 32,
-  }
+    fontSize: 38,
+    width: '100%',
+    height: '100%',
+    marginLeft: 0,
+    marginTop: 5,
+  },
+  inputContainer: { 
+    backgroundColor: 'white', 
+    alignItems: 'center', 
+    height: '100%',
+    borderRadius: 30,
+  },
 });
 
