21b810b6b9954dcb489ed4095aeca268dbb9d555
Brady Bridges <bradyjbridges@gmail.com>
2020-01-05 15:11:10 -0700

    Update taco search and restaurant card tests

--- src/components/RestaurantCard/RestaurantCard.test.js
+++ src/components/RestaurantCard/RestaurantCard.test.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import RestaurantCard from '../RestaurantCard/RestaurantCard';
 
 describe('RestaurantCard', () => {
+  const mockHandlePress = jest.fn();
+  const wrapper = shallow(<RestaurantCard
+    name="Taco Spot"
+    address="123 drive"
+    isClosed={false}
+    img="https://some-url-for-the-image/asld.jpg"
+    handlePress={mockHandlePress}
+    distance="1235.5"
+    id="1"
+  />)
   it('renders correctly', () => {
     const wrapper = renderer.create(<RestaurantCard />).toJSON();
     expect(wrapper).toMatchSnapshot();
+  });
+  it('when a user clicks the card handle press should be called', () => {
+    wrapper.simulate('press');
+    expect(mockHandlePress).toHaveBeenCalledWith("1");
   });
 });
