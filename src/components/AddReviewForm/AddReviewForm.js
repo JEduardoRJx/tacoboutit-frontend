60c30058f086b884f8a9a1ab2e6bbb2c8eab5714
Eduardo <jeduardorjx@gmail.com>
2020-01-06 18:35:21 -0700

    Add handleChange methods for meat type and rating

--- src/components/AddReviewForm/AddReviewForm.js
+++ src/components/AddReviewForm/AddReviewForm.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import { Text, View, Image, StyleSheet, Dimensions, Linking, TouchableOpacity, ScrollView, Modal, Button, Picker } from 'react-native';
+import { Text, View, Image, StyleSheet, Dimensions, Linking, TouchableOpacity, ScrollView, Modal, Button, Picker, TextInput } from 'react-native';
+// import { TextInput } from 'react-native-gesture-handler';
 
 export default class AddReviewForm extends Component {
+  state = {
+    tacoId: this.props.tacos[0].id,
+    rating: 1,
+    review: '',
+    type: this.props.tacos[0].type
+  }
 
   renderTacoTypes = () => {
     const { tacos } = this.props
-    return tacos.map(taco => <Picker.Item label={taco.type} value={taco.type.toLowerCase()} />)
+    return tacos.map(taco => <Picker.Item label={taco.type} value={taco.type.toLowerCase()} key={taco.id} />)
   }
 
   renderRatings = () => {
@@ -22,14 +29,38 @@ export default class AddReviewForm extends Component {
     return ratings
   }
 
+  handleTacoTypeChange = (itemValue, itemIndex) => {
+    const id = this.props.tacos[itemIndex].id
+    this.setState({tacoId: id, type: itemValue})
+  }
+
+  handleRatingChange = (itemValue) => {
+    this.setState({rating: Number(itemValue)})
+  }
+
   render() {
+    console.log(this.state)
     return (
       <View style={{flex: 2}}>
         <Text>Add Review</Text>
         <Text>Type:</Text>
-        <Picker>{this.renderTacoTypes()}</Picker>
+        <Picker
+          onValueChange={(itemValue, itemIndex) => this.handleTacoTypeChange(itemValue, itemIndex)}>{this.renderTacoTypes()}</Picker>
         <Text>Rating:</Text>
-        <Picker>{this.renderRatings()}</Picker>
+        <Picker 
+        selectedValue={this.state.rating}
+        onValueChange={(itemValue) => this.handleRatingChange(itemValue)}
+        >{this.renderRatings()}</Picker>
+        <Text>Review:</Text>
+        <TextInput
+          multiline={true}
+          numberOfLines={4}
+          maxLength={50}
+          onChangeText={(text) => {
+            this.setState({review: text})
+          }}
+          />
+        <Button title='Submit' />
       </View>
     )
   }
