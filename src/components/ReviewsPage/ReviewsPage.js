a04d2f2c5bce6d34c7a487114cfef88b0ce8ef63
Eduardo <jeduardorjx@gmail.com>
2020-01-06 17:42:35 -0700

    Render a review form for a user to make a new review

--- src/components/ReviewsPage/ReviewsPage.js
+++ src/components/ReviewsPage/ReviewsPage.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
-import { Text, View, Image, StyleSheet, Dimensions, Linking, TouchableOpacity, ScrollView, Modal } from 'react-native';
+import { Text, View, Image, StyleSheet, Dimensions, Linking, TouchableOpacity, ScrollView, Modal, Button } from 'react-native';
 import ReviewCard from '../ReviewCard/ReviewCard';
+import AddReviewForm from '../AddReviewForm/AddReviewForm'
+
 export default class ReviewsPages extends Component {
+  state = {
+    toggleForm: false,
+  }
+
   renderReviewCards = () => {
     const { tacos } = this.props;
     return tacos.map((taco) => {
       return <ReviewCard taco={taco} key={taco.id}/>;
     });
   }
+
+  toggleReviewForm = () => {
+    this.setState({toggleForm: !this.state.toggleForm})
+  }
   
   render() {
     const { tacos } = this.props;
     console.log(tacos);
     return (
-      <ScrollView>
-      <View>
+      <ScrollView style={{height: Dimensions.get('window').height}}>
+      <View style={{height: Dimensions.get('window').height}}>
         {this.renderReviewCards()}
+        <TouchableOpacity onPress={() => this.toggleReviewForm ()}>
+          <View style={{height: 50, backgroundColor: '#00BFFF', width: '100%'}}>
+            {this.state.toggleForm === true ? <Text style={{color: 'white', fontSize: 16}}>Cancel</Text> : <Text style={{color: 'white', fontSize: 16}}>+ Review</Text>}
+          </View>
+        </TouchableOpacity>
+        {this.state.toggleForm && <AddReviewForm  tacos={tacos}/>}
       </View>
       </ScrollView>
     )
