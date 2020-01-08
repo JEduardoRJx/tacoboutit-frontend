3478e8e4ac6c53d4e8790556f2bf453fa2b44ffc
Eduardo <jeduardorjx@gmail.com>
2020-01-08 13:06:15 -0700

    Set header and padding for each meat type

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -14,17 +14,27 @@ export default class ReviewCard extends Component {
 
   render() {
     const { taco } = this.props;
-    const rating = taco.average_rating === null ? <Text>No ratings yet</Text> : <Text>Avg Rating: {taco.average_rating}/10</Text>
+    const rating = taco.average_rating === null ? <Text>No ratings yet</Text> : <Text>{taco.average_rating}/10 Avg.</Text>
     return (
       <View style={{backgroundColor: 'lightblue'}}>
-        <Text style={{backgroundColor: 'tomato', textAlgin: 'center'}}>{taco.type.toUpperCase()} {rating}</Text>
-        {/* { taco.average_rating === null ? <Text>No ratings yet</Text> : <Text>Average Rating: {taco.average_rating}/10</Text>} */}
-        {!this.props.taco.reviews.length ? <Text>Be the first to add a review</Text> : this.renderReviews()}
+        <View style={{backgroundColor: 'blue', marginTop: 15}}>
+          <View style={{flexDirection: 'row', width: '100%', justifyContent: 'center'}}>
+            <Text style={styles.textHeader}>{taco.type.toUpperCase()}</Text>
+            <Text style={styles.textHeader}>{rating}</Text>
+            </View>
+          </View>
+          <View style={{backgroundColor: 'green', padding: 10}}>
+            {!this.props.taco.reviews.length ? <Text>Be the first to add a review</Text> : this.renderReviews()}
+          </View>
+        {/* <Text style={{backgroundColor: 'tomato', textAlign: 'center', color: 'white', justifyContent: 'space-between', fontSize: 22}}>{taco.type.toUpperCase()} {rating}</Text> */}
+
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-
+  textHeader: {
+    // backgroundColor: 'tomato', 
+    textAlign: 'center', color: 'white', justifyContent: 'space-between', fontSize: 22, flex: 1, fontWeight: 'bold'}
 })
