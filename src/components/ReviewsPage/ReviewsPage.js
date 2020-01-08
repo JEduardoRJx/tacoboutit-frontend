0bad5b9f09f795b0944739d578e651ba486a10f9
Eduardo <jeduardorjx@gmail.com>
2020-01-07 20:41:37 -0700

    Change out the containers height

--- src/components/ReviewsPage/ReviewsPage.js
+++ src/components/ReviewsPage/ReviewsPage.js
@@ -22,17 +22,29 @@ export default class ReviewsPages extends Component {
   render() {
     const { tacos, updateLocalReviews } = this.props;
     return (
-      <ScrollView style={{height: Dimensions.get('window').height}}>
-        <View style={{height: Dimensions.get('window').height}}>
-          {this.renderReviewCards()}
+      <View style={styles.container}>
+        <View style={{flex: 10, backgroundColor: 'green'}}>
+          {/* {this.renderReviewCards()}
           <TouchableOpacity onPress={() => this.toggleReviewForm ()}>
             <View style={{height: 50, backgroundColor: '#00BFFF', width: '100%'}}>
               {this.state.toggleForm === true ? <Text style={{color: 'white', fontSize: 16}}>Cancel</Text> : <Text style={{color: 'white', fontSize: 16}}>+ Review</Text>}
             </View>
           </TouchableOpacity>
-          {(this.state.toggleForm && tacos.length > 0) &&  <AddReviewForm tacos={tacos} updateLocalReviews={updateLocalReviews} />}
+          {(this.state.toggleForm && tacos.length > 0) &&  <AddReviewForm tacos={tacos} updateLocalReviews={updateLocalReviews} />} */}
         </View>
-      </ScrollView>
+          <TouchableOpacity onPress={() => this.toggleReviewForm ()} style={styles.buttonStyle}>
+            {this.state.toggleForm ? <Text style={styles.buttonTextStyle}>Cancel</Text> : <Text style={styles.buttonTextStyle}>Review</Text>}
+          </TouchableOpacity>
+      </View>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {height: Dimensions.get('window').height, backgroundColor: 'lightblue'},
+  buttonStyle: {height: '100%', backgroundColor: '#00BFFF', width: '100%', justifyContent: 'center', 
+  alignItems: 'center', flex: 1},
+  buttonTextStyle: {color: 'white', fontSize: 32}
+
+})
