5767df8c2e67d7e03a4bb9705a8933c8298283a3
Eduardo <jeduardorjx@gmail.com>
2020-01-08 13:14:40 -0700

    Setting background color for each review card

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -16,14 +16,14 @@ export default class ReviewCard extends Component {
     const { taco } = this.props;
     const rating = taco.average_rating === null ? <Text>No ratings yet</Text> : <Text>{taco.average_rating}/10 Avg.</Text>
     return (
-      <View style={{backgroundColor: 'lightblue'}}>
-        <View style={{backgroundColor: 'blue', marginTop: 15}}>
+      <View style={styles.reviewCardStyling}>
+        <View style={{backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
           <View style={{flexDirection: 'row', width: '100%', justifyContent: 'center'}}>
             <Text style={styles.textHeader}>{taco.type.toUpperCase()}</Text>
             <Text style={styles.textHeader}>{rating}</Text>
             </View>
           </View>
-          <View style={{backgroundColor: 'green', padding: 10}}>
+          <View style={{ padding: 10}}>
             {!this.props.taco.reviews.length ? <Text>Be the first to add a review</Text> : this.renderReviews()}
           </View>
         {/* <Text style={{backgroundColor: 'tomato', textAlign: 'center', color: 'white', justifyContent: 'space-between', fontSize: 22}}>{taco.type.toUpperCase()} {rating}</Text> */}
@@ -34,6 +34,7 @@ export default class ReviewCard extends Component {
 }
 
 const styles = StyleSheet.create({
+  reviewCardStyling: {backgroundColor: 'rgba(225, 225, 225, 0.3)', marginTop: 15, margin: 20, borderRadius: 20, overflow: 'hidden'},
   textHeader: {
     // backgroundColor: 'tomato', 
     textAlign: 'center', color: 'white', justifyContent: 'space-between', fontSize: 22, flex: 1, fontWeight: 'bold'}
